f6bfd82df23104438dce931fc438d03f9223c5e5
Paul Koerbitz <paul.koerbitz@gmail.com>
2011-07-26 12:18:22 +0200

    Fixed some bugs in the views

--- src/jurenal/models.clj
+++ src/jurenal/models.clj
@@ -1,9 +1,10 @@
 (ns jurenal.models 
   (:require [appengine-magic.services.datastore :as ds])
   (:import (play.templates JavaExtensions)
-           (java.util Date)))
+           (java.util Date)
+           (com.google.appengine.api.datastore Text)))
 
-(ds/defentity Post [^:key slug, title, body, short
+(ds/defentity Post [^:key slug, title, ^Text body, short,
                     ^Boolean published, ^Date created-on, ^Date last-changed])
 
 (defn take-words
@@ -11,11 +12,17 @@
   [n s]
   (apply str (take n (re-seq #"\W*\w+" s))))
 
+(defn elide-after [n s]
+  (let [elided (take-words n s)]
+    (if (= elided s)
+      elided
+      (str elided " ..."))))
+
 (defn create [{title :title body :body short :short published :published}]
   (let [created-on (Date.)
         slug (JavaExtensions/slugify title)
-        short (if short short (take-words 50 body))]
-    (ds/save! (Post. slug title body short published created-on created-on))))
+        short (if short short (elide-after 50 body))]
+    (ds/save! (Post. slug title (Text. body) short published created-on created-on))))
 
 (defn fetch [slug]
   (let [post (first (ds/query :kind Post :filter (= :slug slug)))]
@@ -23,12 +30,12 @@
     post))
 
 (defn fetch-all []
-  (ds/query :kind Post))
+  (ds/query :kind Post :sort [[:last-changed :dsc]]))
 
 (defn update [{slug :slug title :title body :body}]
   (let [post (fetch slug)
         now (Date.)]
-    (ds/save! (assoc post :title title :body body :last-changed now))))
+    (ds/save! (assoc post :title title :body (Text. body) :last-changed now))))
 
 (defn delete [slug]
   (ds/delete! (ds/query :kind Post :filter (= :slug slug))))
